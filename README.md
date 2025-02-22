088ee48a68076b8b2098a0f1ea9ebd7ceed619e2
Виталий <ipbvv@mail.ru>
2025-02-22 04:51:15 +0300

    Finish step_4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Vitaliy-Berezhnoy/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Vitaliy-Berezhnoy/python-project-50/actions)
+
+[![asciicast](https://asciinema.org/a/XLpHkUOFny189za3TVRnol8VB.svg)](https://asciinema.org/a/XLpHkUOFny189za3TVRnol8VB)
--- diff_calc/gendiff.py
+++ diff_calc/gendiff.py
@@ -10,15 +10,30 @@ def prepare_arguments():
     args = parser.parse_args()
     dir_and_name_file1 = args.first_file
     dir_and_name_file2 = args.second_file
-    out_format = args.format
-    print(dir_and_name_file1)
-    print(dir_and_name_file2)
-    print(out_format)
+#    out_format = args.format
     generate_diff(dir_and_name_file1, dir_and_name_file2)
 
 
-def generate_diff(name_file1, name_file2):
+def generate_diff(name_file1: str, name_file2: str):
     file1 = json.load(open(name_file1))
     file2 = json.load(open(name_file2))
     print(file1)
     print(file2)
+
+    def inner(key: str):
+        if key not in file2:
+            return f'  - {key}: {file1[key]}'
+        if file1[key] == file2[key]:
+            return f'    {key}: {file1[key]}'
+        return f'  -{key}: {file1[key]}\n  +{key}: {file2[key]}'
+
+    key_file1 = sorted(file1)
+    key_file2 = sorted(file2)
+    out_list = ['{']
+    out_list.extend(list(map(inner, key_file1)))
+    new_key = set(key_file2) - set(key_file1)
+#    new_key = filter(lambda key: key not in key_file1, key_file2)
+    out_list.extend(list(map(lambda key: f'  +{key}: {file2[key]}', new_key)))
+    out_list.append('}')
+    print('\n'.join(out_list))
+    return '\n'.join(out_list)    
