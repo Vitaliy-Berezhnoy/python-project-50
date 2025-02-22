bc97fd3ced882648bcbaa6a2849cef9d04d0f6ea
Виталий <ipbvv@mail.ru>
2025-02-22 21:12:09 +0300

    Finish step_5

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Vitaliy-Berezhnoy/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Vitaliy-Berezhnoy/python-project-50/actions)
 
+[![Maintainability](https://api.codeclimate.com/v1/badges/5a748fc185a130ffa0f2/maintainability)](https://codeclimate.com/github/Vitaliy-Berezhnoy/python-project-50/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/5a748fc185a130ffa0f2/test_coverage)](https://codeclimate.com/github/Vitaliy-Berezhnoy/python-project-50/test_coverage)
+
 [![asciicast](https://asciinema.org/a/XLpHkUOFny189za3TVRnol8VB.svg)](https://asciinema.org/a/XLpHkUOFny189za3TVRnol8VB)
--- diff_calc/gendiff.py
+++ diff_calc/gendiff.py
@@ -1,31 +1,38 @@
 import argparse
 import json
 
+# from itertools import chain
+
 
 def prepare_arguments():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+        )
     parser.add_argument('first_file', type=str, help='')
     parser.add_argument('second_file', type=str, help='')
-    parser.add_argument('-f', '--format', type=str, default='.json', help='set format of output')
+    parser.add_argument(
+        '-f', '--format',
+        type=str,
+        default='.json',
+        help='set format of output'
+        )
     args = parser.parse_args()
-    dir_and_name_file1 = args.first_file
-    dir_and_name_file2 = args.second_file
+    path_name_file1 = args.first_file
+    path_name_file2 = args.second_file
 #    out_format = args.format
-    generate_diff(dir_and_name_file1, dir_and_name_file2)
+    print(generate_diff(path_name_file1, path_name_file2))
 
 
-def generate_diff(name_file1: str, name_file2: str):
-    file1 = json.load(open(name_file1))
-    file2 = json.load(open(name_file2))
-    print(file1)
-    print(file2)
+def generate_diff(path_name_file1: str, path_name_file2: str):
+    file1 = json.load(open(path_name_file1))
+    file2 = json.load(open(path_name_file2))
 
     def inner(key: str):
         if key not in file2:
             return f'  - {key}: {file1[key]}'
         if file1[key] == file2[key]:
             return f'    {key}: {file1[key]}'
-        return f'  -{key}: {file1[key]}\n  +{key}: {file2[key]}'
+        return f'  - {key}: {file1[key]}\n  + {key}: {file2[key]}'
 
     key_file1 = sorted(file1)
     key_file2 = sorted(file2)
@@ -33,7 +40,29 @@ def generate_diff(name_file1: str, name_file2: str):
     out_list.extend(list(map(inner, key_file1)))
     new_key = set(key_file2) - set(key_file1)
 #    new_key = filter(lambda key: key not in key_file1, key_file2)
-    out_list.extend(list(map(lambda key: f'  +{key}: {file2[key]}', new_key)))
+    out_list.extend(list(map(lambda key: f'  + {key}: {file2[key]}', new_key)))
     out_list.append('}')
-    print('\n'.join(out_list))
-    return '\n'.join(out_list)    
+    result = '\n'.join(out_list)
+    result = result.replace('True', 'true')
+    result = result.replace('False', 'false')
+    return result
+
+#    def inner(key: str):
+#        if key not in file2:
+#            return [(f'- {key}', file1[key])]
+#        if file1[key] == file2[key]:
+#            return [(f'  {key}', file1[key])]
+#        return [(f'- {key}', file1[key]), (f'+ {key}', file2[key])]
+#
+#    key_file1 = sorted(file1)
+#    key_file2 = sorted(file2)
+#    out_list = list(map(inner, key_file1))
+#    new_key = set(key_file2) - set(key_file1)
+#    out_list.extend(list(map(lambda key: [(f'+ {key}', file2[key])], new_key)))
+#    out_dict = dict(chain(*out_list))
+#    print(out_list)
+#    print(out_dict)
+#    result = json.dumps(out_dict, indent=2)
+#    result = result.replace('"', '')
+#    print(result)
+#    return result
